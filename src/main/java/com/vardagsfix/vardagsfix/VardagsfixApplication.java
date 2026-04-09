4495f90aa553daebe4499182e92841120d33b042
ceciliachris <cecilia.christianssons@gmail.com>
2026-04-09 09:32:33 +0200

    Initial Spring Boot setup

---
+++ src/main/java/com/vardagsfix/vardagsfix/VardagsfixApplication.java
@@ -0,0 +1,13 @@
+package com.vardagsfix.vardagsfix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VardagsfixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VardagsfixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vardagsfix/vardagsfix/VardagsfixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vardagsfix.vardagsfix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VardagsfixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
